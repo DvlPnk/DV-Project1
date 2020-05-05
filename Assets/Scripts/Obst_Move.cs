c64b29a0b6e676b4cb5ac79dc045aa585297716e
Pierre Ruiz <52357318+DvlPnk@users.noreply.github.com>
2020-05-05 17:02:16 -0500

    Add files via upload

---
+++ Assets/Scripts/Obst_Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obst_Move : MonoBehaviour
+{
+    Rigidbody rbd;
+    bool ret, trig, st;
+    public float time_1, time_max = 2.0f, vel = 10.0f;
+    private float posx, posz;
+    private int i = 1, j = 1;
+    void Start()
+    {
+        rbd = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        rbd.velocity = new Vector3(-1, 0, 0)*vel;
+    }
+    private void FixedUpdate()
+    {
+
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
