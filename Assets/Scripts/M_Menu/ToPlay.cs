1fc45a1198ed33c648dd65757bdd49482c6c9538
Pierre Ruiz <52357318+DvlPnk@users.noreply.github.com>
2020-05-14 18:54:46 -0500

    V2.2

---
+++ Assets/Scripts/M_Menu/ToPlay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToPlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool Playing = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Play()
+    {
+        Playing = true;
+        SceneManager.LoadScene("Tut");
+    }
+}
---
+++ Assets/Scripts/S_Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Movement : MonoBehaviour
+{
+    Rigidbody rbd;
+    bool ret;
+    public float time_1, time_max = 2.0f, vel = 10.0f;
+    private float posz;
+    private int i = 1, j=1;
+    void Start()
+    {
+        rbd = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        rbd.velocity = new Vector3(-1, 0, 0) *vel;
+        if (ret)
+        {
+            if (i == j)
+            {
+                posz = Random.Range(11.0f, -11.0f);
+                j++;
+            }
+            rbd.transform.position = new Vector3(23.12f, .5f, posz);
+            if ((Time.time - time_1) >= time_max)
+            {   
+                ret = false;
+                i++;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            ret = true;
+            time_1 = Time.time;
+        }
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            ret = true;
+            time_1 = Time.time;
+        }
+    }
+}
