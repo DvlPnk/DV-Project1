635e68afed7bdc895ff3473117cc0ce91251f0e9
Pierre Ruiz <52357318+DvlPnk@users.noreply.github.com>
2020-05-14 21:38:03 -0500

    V 2.3

---
+++ Assets/Scripts/Nivel_1/Nivel_1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Nivel_1 : MonoBehaviour
+{
+    private float time1, time2= 1.0f;
+    private int i = 1;
+    public P_Movement p_move;
+    public GameObject nivel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+        time1 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time1 < time2)
+        {
+            time1 += 0.1f;
+            i = 0;
+        }
+        if (time1 >= time2 && i==0) {
+            Time.timeScale = 1;
+            nivel.SetActive(false);
+            i = 1;
+        }
+        if(p_move.points == 5)
+        {
+            SceneManager.LoadScene("Nivel_2");
+        }
+    }
+}
---
+++ Assets/Scripts/Tuto/Obts_MoveT_N1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obts_MoveT_N1 : MonoBehaviour
+{
+    Rigidbody rbd;
+    bool ret;
+    public float time_1, vel = 10.0f;
+    private float posz;
+    private int i = 1, j = 1;
+    void Start()
+    {
+        rbd = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        rbd.velocity = new Vector3(-1, 0, 0) * vel;
+        if (ret)
+        {
+            if (i == j)
+            {
+                posz = Random.Range(11.0f, -11.0f);
+                j++;
+            }
+            rbd.transform.position = new Vector3(25.4f, .5f, posz);
+            ret = false;
+            i++;
+        }
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            ret = true;
+            time_1 = Time.time;
+        }
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            ret = true;
+            time_1 = Time.time;
+        }
+    }
+}
