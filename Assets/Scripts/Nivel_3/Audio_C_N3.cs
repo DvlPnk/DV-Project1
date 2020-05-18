57d050f0531123af1adc866e853063e196965c8a
Pierre Ruiz <52357318+DvlPnk@users.noreply.github.com>
2020-05-18 12:22:34 -0500

    V 2.5

---
+++ Assets/Scripts/Nivel_3/Audio_C_N3.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_C_N3 : MonoBehaviour
+{
+    public GameObject Fast;
+    public GameObject Slow;
+    public GameObject Crash;
+    public P_M_N3 player;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (player.temp == 1)
+        {
+            Slow.SetActive(true);
+        }
+        if (player.temp == 2 || player.temp == 3)
+        {
+            Fast.SetActive(true);
+        }
+        if (player.temp == 0)
+        {
+            Slow.SetActive(false);
+            Fast.SetActive(false);
+        }
+        if(player.vida==1) {
+            Crash.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Nivel_T-2/Audio_C.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_C : MonoBehaviour
+{
+    public GameObject Fast;
+    public GameObject Slow;
+    public GameObject Crash;
+    public P_Movement player;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (player.temp == 1)
+        {
+            Slow.SetActive(true);
+        }
+        if (player.temp == 2 || player.temp == 3)
+        {
+            Fast.SetActive(true);
+        }
+        if (player.temp == 0)
+        {
+            Slow.SetActive(false);
+            Fast.SetActive(false);
+        }
+        if (player.vida == 1)
+        {
+            Crash.SetActive(true);
+        }
+    }
+}
